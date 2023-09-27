8cf27cada7b315556338f036270e513df2697102
Abhishek Kumar <litecode420@gmail.com>
2023-09-27 02:57:57 +0000

    DataType

---
+++ DataType.js
@@ -0,0 +1,22 @@
+"use strict"  // it will allowed modern standerd of code
+
+// how many data type is there
+
+
+//Number ---- 2 to power 53
+// string
+//bitInt
+//undefined
+//null
+//symbol
+
+
+let myName = "Abhishek";   // it is string 
+
+console.log(typeof null);
+console.log(typeof myName);
+console.log(typeof undefined);
+
+// undefined and null both are defferent things don't mix them
+
+console.log(typeof NaN);
