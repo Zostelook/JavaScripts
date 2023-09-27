36ab66ee23f7c28dbe4b40f9f555ab81fb2b1e74
Abhishek Kumar <litecode420@gmail.com>
2023-09-27 01:46:19 +0000

    Variables

---
+++ Variable.js
@@ -0,0 +1,8 @@
+const accountId = "3874838473498";
+let accountName = "Abhishek";
+var accountState = "Delhi";
+
+
+// accountId = "423847448";   we can't change the value of constant entire the program
+
+console.table([accountId,accountName,accountState]);
