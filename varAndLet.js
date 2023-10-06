36964a678779287004c84cd3fdd9514fb5a2a2c5
Abhishek Kumar <litecode420@gmail.com>
2023-10-06 03:51:42 +0000

    var

---
+++ varAndLet.js
@@ -0,0 +1,25 @@
+
+//
+var collage = "IITM";
+
+let nameX ="abhi"   // global scope
+
+
+function greet(){
+
+    var lname = 'jha'  //
+    let fname = "yadav";   // local scope
+    console.log(nameX)
+
+
+
+
+}
+
+//greet();  
+
+console.log(lname)
+
+
+
+
